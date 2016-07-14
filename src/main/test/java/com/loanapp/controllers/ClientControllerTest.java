5d5b044e494d714f1015377ba738415dbd9e526f
Aleksej Zaharenko <aleksej.zaharenko@avocodo.com>
2016-07-14 16:51:42 +0300

    Unit tests

--- src/main/test/java/com/loanapp/controllers/ClientControllerTest.java
+++ src/main/test/java/com/loanapp/controllers/ClientControllerTest.java
@@ -6,20 +6,21 @@ import com.loanapp.repositories.ClientRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -32,20 +33,119 @@ public class ClientControllerTest {
 
     ClientController instance;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
     @Before
     public void setup() {
       instance = new ClientController();
       instance.clientRepository = mock(ClientRepository.class);
+      mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
     }
 
     @Test
-    public void testFindClientById() {
-//      when(instance.clientRepository.findById(ID), (MockHttpServletRequest) anyObject())).thenReturn("url");
-//
-//      assertEquals("url", instance.start(new MockHttpServletRequest(), "85ee6697-0904-4b50-8072-5661dd61152e"));
-//
-//      verify(instance.clientRepository).findById(ID)(eq("85ee6697-0904-4b50-8072-5661dd61152e"), (MockHttpServletRequest) anyObject());
+    public void testFindClientById() throws Exception {
+      Client client = new Client(ID, FIRST_NAME, LAST_NAME);
+      when(instance.clientRepository.findById(client.getId())).thenReturn(client);
+
+      mockMvc.perform(get("/client/{id}", client.getId()))
+          .andExpect(status().isOk())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+          .andExpect(jsonPath("$.id").value(client.getId()))
+          .andExpect(jsonPath("$.firstName").value(client.getFirstName()))
+          .andExpect(jsonPath("$.lastName").value(client.getLastName()));
+
+
+      verify(instance.clientRepository, times(1)).findById(client.getId());
       verifyNoMoreInteractions(instance.clientRepository);
+    }
+
+    @Test
+    public void testFindClientById1() throws Exception {
+      when(instance.clientRepository.findById(anyString())).thenReturn(null);
+
+      mockMvc.perform(get("/client/{id}", anyString()))
+          .andExpect(status().isNotFound())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+          .andExpect(jsonPath("$.type").value("INFO"))
+          .andExpect(jsonPath("$.message").value("Client not found"));
+
+      verify(instance.clientRepository, times(1)).findById(anyString());
+      verifyNoMoreInteractions(instance.clientRepository);
+    }
+
+    @Test
+    public void testBlacklistClient() throws Exception {
+      Client client = new Client(ID, FIRST_NAME, LAST_NAME);
+      when(instance.clientRepository.findById(client.getId())).thenReturn(client);
+      mockMvc.perform(put("/customer/blacklist/add/{id}", client.getId()))
+          .andExpect(status().isOk());
+
+      verify(instance.clientRepository.findById(client.getId()).isBlacklisted());
+    }
+
+    @Test
+    public void testBlacklistClient1() throws Exception {
+      when(instance.clientRepository.findById(anyString())).thenReturn(null);
+      mockMvc.perform(put("/customer/blacklist/add/{id}", anyString()))
+          .andExpect(status().isNotFound())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+          .andExpect(jsonPath("$.type").value("INFO"))
+          .andExpect(jsonPath("$.message").value("Client not found"));
+    }
+
+    @Test
+    public void testBlacklistClient2() throws Exception {
+      Client client = new Client(ID, FIRST_NAME, LAST_NAME);
+      client.setBlacklisted(true);
+      when(instance.clientRepository.findById(client.getId())).thenReturn(client);
+
+      mockMvc.perform(put("/customer/blacklist/add/{id}", client.getId()))
+          .andExpect(status().isAlreadyReported())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+          .andExpect(jsonPath("$.type").value("INFO"))
+          .andExpect(jsonPath("$.message").value("Client already blacklisted"));
+
+      verify(instance.clientRepository.findById(client.getId()).isBlacklisted());
+    }
+
+    @Test
+    public void testRemoveFromBlacklistClient() throws Exception {
+      Client client = new Client(ID, FIRST_NAME, LAST_NAME);
+      client.setBlacklisted(true);
+      when(instance.clientRepository.findById(client.getId())).thenReturn(client);
+
+      mockMvc.perform(put("/customer/blacklist/remove/{id}", client.getId()))
+          .andExpect(status().isOk());
+
+      verify(!instance.clientRepository.findById(client.getId()).isBlacklisted());
+    }
+
+    @Test
+    public void testRemoveFromBlacklistClient1() throws Exception {
+      when(instance.clientRepository.findById(anyString())).thenReturn(null);
+
+      mockMvc.perform(put("/customer/blacklist/remove/{id}", anyString()))
+          .andExpect(status().isNotFound())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+          .andExpect(jsonPath("$.type").value("INFO"))
+          .andExpect(jsonPath("$.message").value("Client not found"));
+    }
+
+    @Test
+    public void testBlacklistClient3() throws Exception {
+      Client client = new Client(ID, FIRST_NAME, LAST_NAME);
+      client.setBlacklisted(false);
+      when(instance.clientRepository.findById(client.getId())).thenReturn(client);
+
+      mockMvc.perform(put("/customer/blacklist/remove/{id}", client.getId()))
+          .andExpect(status().isAlreadyReported())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+          .andExpect(jsonPath("$.type").value("INFO"))
+          .andExpect(jsonPath("$.message").value("Client not in blacklist"));
+
+      verify(instance.clientRepository.findById(client.getId()).isBlacklisted());
     }
 
 }
---
+++ src/main/test/java/com/loanapp/controllers/LoanControllerTest.java
@@ -0,0 +1,5 @@
+package com.loanapp.controllers;
+
+
+public class LoanControllerTest {
+}
