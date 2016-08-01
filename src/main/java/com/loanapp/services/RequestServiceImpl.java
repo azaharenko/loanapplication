ed756b5099597f44df7bbafd99d14c0c50d69a3a
Aleksej Zaharenko <aleksej.zaharenko@avocodo.com>
2016-08-01 15:36:30 +0300

    remove unnecessary service name annotation

--- src/main/java/com/loanapp/services/RequestServiceImpl.java
+++ src/main/java/com/loanapp/services/RequestServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 
-@Service("requestService")
+@Service
 public class RequestServiceImpl implements RequestService {
     protected final static Logger log = LogManager.getLogger(RequestServiceImpl.class);
 
